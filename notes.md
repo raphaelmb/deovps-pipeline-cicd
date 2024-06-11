8cc55bc44c5919677cc9c06e9ee314ac30a5fdea
Raphael Berger <raphaelmb@gmail.com>
2024-06-11 13:18:37 -0300

    First commit

---
+++ notes.md
@@ -0,0 +1,5 @@
+### Steps
+
+1. auth on docker hub (login)
+2. docker build
+3. docker push
---
+++ src/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		fmt.Fprintf(w, "Example app")
+	})
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
